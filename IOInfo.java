612742ea2ed2f5f823d80c6fc22a3ab4776d2c41
rajaponna <raja.ponna1@marist.edu>
2017-04-23 21:54:09 -0400

    Updated code with few changes

--- IOInfo.java
+++ IOInfo.java
@@ -10,77 +10,77 @@ package com.mscs710.taskmanager.StatsInfo;
  * @author raviteja, pooja, akshara
  */
 public class IOInfo {
-    String IOInfo_DISKNAME;
-    double IOInfo_TRANSFERRATEPERSEC;
-    double IOInfo_KB_READS;
-    double IOInfo_KB_WRITES;
-    double IOInfo_BLOCKREADS;
-    double IOInfo_BLOCKWRITES;
+    String IOInformation_DISKNAME;
+    double IOInformation_TRANSFERRATEPERSEC;
+    double IOInformation_KB_READS;
+    double IOInformation_KB_WRITES;
+    double IOInformation_BLOCKREADS;
+    double IOInformation_BLOCKWRITES;
 
-    /* getIOInfo_DISKNAME */
-    /* setIOInfo_DISKNAME  */
+    /* getIOInformation_DISKNAME */
+    /* setIOInformation_DISKNAME  */
     
-    public String getIOInfo_DISKNAME() {
-        return IOInfo_DISKNAME;
+    public String getIOInformation_DISKNAME() {
+        return IOInformation_DISKNAME;
     }
  
-    public void setIOInfo_DISKNAME(String IOInfo_DISKNAME) {
-        this.IOInfo_DISKNAME = IOInfo_DISKNAME;
+    public void setIOInformation_DISKNAME(String IOInformation_DISKNAME) {
+        this.IOInformation_DISKNAME = IOInformation_DISKNAME;
     }
 
-    /* getIOInfo_TRANSFERRATEPERSEC */
-    /* setIOInfo_TRANSFERRATEPERSEC */
+    /* getIOInformation_TRANSFERRATEPERSEC */
+    /* setIOInformation_TRANSFERRATEPERSEC */
     
-    public double getIOInfo_TRANSFERRATEPERSEC() {
-        return IOInfo_TRANSFERRATEPERSEC;
+    public double getIOInformation_TRANSFERRATEPERSEC() {
+        return IOInformation_TRANSFERRATEPERSEC;
     }
     
-    public void setIOInfo_TRANSFERRATEPERSEC(double IOInfo_TRANSFERRATEPERSEC) {
-        this.IOInfo_TRANSFERRATEPERSEC = IOInfo_TRANSFERRATEPERSEC;
-    }
-
-    /* getIOInfo_KB_READS */
-    /* setIOInfo_KB_READS */
-    
-    public double getIOInfo_KB_READS() {
-        return IOInfo_KB_READS;
-    }
-    
-    public void setIOInfo_KB_READS(double IOInfo_KB_READS) {
-        this.IOInfo_KB_READS = IOInfo_KB_READS;
+    public void setIOInformation_TRANSFERRATEPERSEC(double IOInformation_TRANSFERRATEPERSEC) {
+        this.IOInformation_TRANSFERRATEPERSEC = IOInformation_TRANSFERRATEPERSEC;
     }
 
-    /* getIOInfo_KB_WRITES */
-    /* setIOInfo_KB_WRITES */
+    /* getIOInformation_KB_READS */
+    /* setIOInformation_KB_READS */
     
-    public double getIOInfo_KB_WRITES() {
-        return IOInfo_KB_WRITES;
+    public double getIOInformation_KB_READS() {
+        return IOInformation_KB_READS;
+    }
+    
+    public void setIOInformation_KB_READS(double IOInformation_KB_READS) {
+        this.IOInformation_KB_READS = IOInformation_KB_READS;
+    }
+
+    /* getIOInformation_KB_WRITES */
+    /* setIOInformation_KB_WRITES */
+    
+    public double getIOInformation_KB_WRITES() {
+        return IOInformation_KB_WRITES;
     }
  
-    public void setIOInfo_KB_WRITES(double IOInfo_KB_WRITES) {
-        this.IOInfo_KB_WRITES = IOInfo_KB_WRITES;
+    public void setIOInformation_KB_WRITES(double IOInformation_KB_WRITES) {
+        this.IOInformation_KB_WRITES = IOInformation_KB_WRITES;
     }
 
-    /* getIOInfo_BLOCKREADS */
-    /* setIOInfo_BLOCKREADS */
+    /* getIOInformation_BLOCKREADS */
+    /* setIOInformation_BLOCKREADS */
     
-    public double getIOInfo_BLOCKREADS() {
-        return IOInfo_BLOCKREADS;
+    public double getIOInformation_BLOCKREADS() {
+        return IOInformation_BLOCKREADS;
     }
     
-    public void setIOInfo_BLOCKREAD(double IOInfo_BLOCKREADS) {
-        this.IOInfo_BLOCKREADS = IOInfo_BLOCKREADS;
+    public void setIOInformation_BLOCKREADS(double IOInformation_BLOCKREADS) {
+        this.IOInformation_BLOCKREADS = IOInformation_BLOCKREADS;
     }
 
-    /* getIOInfo_BLOCKWRITES */
-    /* setIOInfo_BLOCKWRITES */
+    /* getIOInformation_BLOCKWRITES */
+    /* setIOInformation_BLOCKWRITES */
     
-    public double getIOInfo_BLOCKWRITES() {
-        return IOInfo_BLOCKWRITES;
+    public double getIOInformation_BLOCKWRITES() {
+        return IOInformation_BLOCKWRITES;
     }
     
-    public void setIOInfo_BLOCKWRITES(double IOInfo_BLOCKWRITES) {
-        this.IOInfo_BLOCKWRITES = IOInfo_BLOCKWRITES;
+    public void setIOInformation_BLOCKWRITES(double IOInformation_BLOCKWRITES) {
+        this.IOInformation_BLOCKWRITES = IOInformation_BLOCKWRITES;
     }
     
 }
